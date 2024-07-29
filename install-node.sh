e3bb484cf454456580c6b8b9939243b84c50e226
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-29 21:36:43 +0200

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+read -p "Sure? " yn
+case $yn in
+ y|Y|yes|Yes|YES) ;;
+ *) exit ;;
+esac
+
+#backup config files
+[ -d ~/backup ] || mkdir ~/backup
+[ -d ~/backup/0g-da-node ] || mkdir ~/backup/0g-da-node
+cp ~/0g-da-node/config.toml ~/backup/0g-da-node/config.toml
+
 apt install libssl-dev
 apt install protobuf-compiler
 
 cd ~
+rm -r 0g-da-node
 git clone https://github.com/0glabs/0g-da-node.git
 cd 0g-da-node
 cargo build --release
 ./dev_support/download_params.sh
 cargo run --bin key-gen
 
+#restore config files
+cp ~/backup/0g-da-node/config.toml ~/0g-da-node/config.toml
