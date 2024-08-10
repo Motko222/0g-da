6783f2a58c591bb660e24f6666ee5f46c5ab4a6b
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-10 23:21:01 +0200

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -13,8 +13,7 @@ cp ~/0g-da-node/config.toml ~/backup/0g-da-node/config.toml
 
 apt-get update -y
 apt-get upgrade -y
-apt install libssl-dev -y
-apt install protobuf-compiler -y
+apt install libssl-dev protobuf-compiler build-essential -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
