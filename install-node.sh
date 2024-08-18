97ced43a31e1647e3870edc97e4297175d99cbef
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-18 22:15:42 +0200

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-read -p "Sure? " yn
-case $yn in
- y|Y|yes|Yes|YES) ;;
- *) exit ;;
-esac
-
-#backup config files
-[ -d ~/backup ] || mkdir ~/backup
-[ -d ~/backup/0g-da-node ] || mkdir ~/backup/0g-da-node
-cp ~/0g-da-node/config.toml ~/backup/0g-da-node/config.toml
+read -p "Tag? (https://github.com/0glabs/0g-da-node/releases) " tag
 
 apt-get update -y
 apt-get upgrade -y
@@ -21,10 +12,10 @@ cd ~
 rm -r 0g-da-node
 git clone https://github.com/0glabs/0g-da-node.git
 cd 0g-da-node
-git checkout tags/v1.0.1 -b v1.0.1
+git checkout tags/$tag -b $tag
 cargo build --release
 ./dev_support/download_params.sh
 cargo run --bin key-gen
 
 #restore config files
-cp ~/backup/0g-da-node/config.toml ~/0g-da-node/config.toml
+cp config-example.toml config.toml
