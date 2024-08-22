b0cd2089d3717ca14665ed4ca64d1a01d8497455
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-22 15:32:20 +0200

    Update create-service.sh

--- create-service.sh
+++ create-service.sh
@@ -15,22 +15,26 @@ RestartSec=30
 WantedBy=multi-user.target
 EOF
 
-sudo tee /etc/systemd/system/0g-da-client.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/0g-da-retriever.service > /dev/null <<EOF
 [Unit]
-Description=0g da client
+Description=0G-DA Retriever
 After=network.target
-StartLimitIntervalSec=0
+
 [Service]
 User=root
-WorkingDirectory=/root/0g-da-xxx
-ExecStart=echo
+WorkingDirectory=/root/0g-da-retriever
+ExecStart=/root/0g-da-retriever/target/release/retriever --config /root/0g-da-retriever/run/config.toml
 Restart=always
-RestartSec=30
+RestartSec=10
+LimitNOFILE=65535
+StandardOutput=journal
+StandardError=journal
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable 0g-da-node
-sudo systemctl enable 0g-da-client
+sudo systemctl enable 0g-da-retriever
 
