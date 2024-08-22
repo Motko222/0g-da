4fd3953d8aa1337d517fcfa7f7690ad4ce720ce8
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-22 14:15:23 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -13,13 +13,14 @@ folder_size=$(du -hs -L ~/0g-da-node | awk '{print $1}')
 
 #get RPC addresses
 node_rpc=$(cat ~/0g-da-node/config.toml | grep '^grpc_listen_address =' | tail -1 | awk '{print $3}' | sed 's/"//g')
+node_rpc=$(cat ~/0g-da-client/config.toml | grep '^grpc_listen_address =' | tail -1 | awk '{print $3}' | sed 's/"//g')
 
 #get da node info
 cd ~/0g-da-node/target/release
 node_version=$(./server --version | awk '{print $2}')
 cd ~
-json=$(./grpcurl --plaintext --import-path ~/ --proto ~/signer.proto $node_rpc signer.Signer/GetStatus)
-node_status=$(echo $json | jq -r .statusCode)
+node_status=$(./grpcurl --plaintext --import-path ~/ --proto ~/signer.proto $node_rpc signer.Signer/GetStatus | jq -r .statusCode)
+disperser_status=$(./grpcurl --plaintext localhost:51001 grpc.health.v1.Health/Check | jq -r .status )
 
 cat << EOF
 {
@@ -35,7 +36,8 @@ cat << EOF
    { "key":"folder_size","value":"$folder_size" },
    { "key":"node_rpc","value":"$node_rpc" },
    { "key":"node_version","value":"$node_version" },
-   { "key":"node_status","value":"$node_status" }
+   { "key":"node_status","value":"$node_status" },
+   { "key":"disperser_status","value":"$disperser_status" }
   ]
 }
 EOF
@@ -49,6 +51,6 @@ then
   --header "Content-Type: text/plain; charset=utf-8" \
   --header "Accept: application/json" \
   --data-binary "
-    report,machine=$MACHINE,id=$ID,grp=$grp,owner=$OWNER status=\"$status\",message=\"$message\",node_version=\"$node_version\",node_rpc=\"$node_rpc\",chain=\"$chain\",node_status=\"$node_status\" $(date +%s%N) 
+    report,machine=$MACHINE,id=$ID,grp=$grp,owner=$OWNER status=\"$status\",message=\"$message\",node_version=\"$node_version\",node_rpc=\"$node_rpc\",chain=\"$chain\",node_status=\"$node_status\",disperser_status=\"$disperser_status\" $(date +%s%N) 
     "
 fi
