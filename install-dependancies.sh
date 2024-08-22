be2737631f0029ebb9feb19c22666e9571a9740c
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-22 21:31:09 +0200

    Update install-dependancies.sh

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -18,3 +18,11 @@ go version
 #install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
+
+#install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
