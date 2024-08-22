fc7f4868b6d9d48d32581560f6720269160cf403
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-22 14:16:17 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -13,14 +13,14 @@ folder_size=$(du -hs -L ~/0g-da-node | awk '{print $1}')
 
 #get RPC addresses
 node_rpc=$(cat ~/0g-da-node/config.toml | grep '^grpc_listen_address =' | tail -1 | awk '{print $3}' | sed 's/"//g')
-node_rpc=$(cat ~/0g-da-client/config.toml | grep '^grpc_listen_address =' | tail -1 | awk '{print $3}' | sed 's/"//g')
+disperser_rpc=localhost:51001
 
 #get da node info
 cd ~/0g-da-node/target/release
 node_version=$(./server --version | awk '{print $2}')
 cd ~
 node_status=$(./grpcurl --plaintext --import-path ~/ --proto ~/signer.proto $node_rpc signer.Signer/GetStatus | jq -r .statusCode)
-disperser_status=$(./grpcurl --plaintext localhost:51001 grpc.health.v1.Health/Check | jq -r .status )
+disperser_status=$(./grpcurl --plaintext $disperser_rpc grpc.health.v1.Health/Check | jq -r .status )
 
 cat << EOF
 {
