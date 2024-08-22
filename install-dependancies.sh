7f0d2339d93ad724e5df410f3177a90184d81644
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-22 21:29:26 +0200

    Update install-dependancies.sh

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -2,6 +2,19 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt install libssl-dev protobuf-compiler build-essential pkg-config clang -y
+apt install libssl-dev protobuf-compiler build-essential pkg-config clang cmake -y
+
+#install go
+cd $HOME 
+ver="1.22.0"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+#install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
